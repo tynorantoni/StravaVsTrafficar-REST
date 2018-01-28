be2dba536baa0d5cf93d3f95585747587d89e840
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2018-01-28 16:58:28 +0100

    Initial

---
+++ src/main/java/pl/pawelSz/StravaVsTrafficarApp/StravaVsTrafficarApplication.java
@@ -0,0 +1,13 @@
+package pl.pawelSz.StravaVsTrafficarApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaVsTrafficarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravaVsTrafficarApplication.class, args);
+		System.out.println("Hejka");
+	}
+}
